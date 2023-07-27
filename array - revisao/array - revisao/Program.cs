859247f05ead03fa8e9fd06382c0fa275a1080fb
Wendry bergamasco <2022102020033@aluno.ifro.edu.br>
2023-07-27 11:50:15 -0400

    revisao array

---
+++ array - revisao/array - revisao/Program.cs
@@ -0,0 +1,22 @@
+﻿/*int[] array = new int[2];
+Console.WriteLine("digite a primeira nota: ");
+array[0] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("digite a segunda nota");
+array[1] = Convert.ToInt32(Console.ReadLine());
+
+double media = (array[0] + array[1]) / 2;
+Console.WriteLine($"a media é {media}");*/
+
+List<string> Lista1 = new List<string>();
+Lista1.Add("bergamasco");
+Lista1.Add("wendry");
+Lista1.Add("santos");
+
+Console.WriteLine(Lista1[2]);
+
+
+
+foreach (string str in Lista1)
+{
+    Console.WriteLine(str);
+}
